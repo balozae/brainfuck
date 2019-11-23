ee5a331cdb30c63a0f62b4b1968b714f05330f07
balozae <43301216+balozae@users.noreply.github.com>
2019-11-23 23:53:31 +0400

    убрал lodash, main теперь изменяет исходный массив

--- src/sudoku.js
+++ src/sudoku.js
@@ -1,12 +1,3 @@
-const map = require('lodash/map');
-const intersection = require('lodash/intersection');
-const flatten = require('lodash/flatten');
-
-const range = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-// может решать только простые
-
-// easy
 const puzzle = [
   [5, 3, 0, 0, 7, 0, 0, 0, 0],
   [6, 0, 0, 1, 9, 5, 0, 0, 0],
@@ -19,8 +10,8 @@ const puzzle = [
   [0, 0, 0, 0, 8, 0, 0, 7, 9]
 ];
 
-/*
 // easy
+/*
 const puzzle = [
   [1, 0, 0, 0, 0, 7, 0, 5, 9],
   [0, 0, 0, 4, 0, 0, 1, 0, 0],
@@ -31,10 +22,10 @@ const puzzle = [
   [3, 5, 7, 0, 0, 0, 6, 0, 8],
   [0, 0, 2, 0, 0, 5, 0, 0, 0],
   [8, 9, 0, 7, 0, 0, 0, 0, 4]
-];*/
+];
+*/
 
-// не решит
-// medium
+// medium - не решит
 /*
 const puzzle = [
   [1, 0, 0, 0, 5, 0, 6, 0, 0],
@@ -46,22 +37,74 @@ const puzzle = [
   [0, 0, 9, 7, 0, 0, 0, 8, 0],
   [0, 2, 0, 0, 4, 0, 0, 0, 6],
   [0, 0, 8, 0, 3, 0, 0, 0, 9]
-];*/
-
-const SECTION_LIST = [
-  //min: [x, y]  max: [x, y]
-  { min: [0, 0], max: [2, 2] },
-  { min: [3, 0], max: [5, 2] },
-  { min: [6, 0], max: [8, 2] },
-
-  { min: [0, 3], max: [2, 5] },
-  { min: [3, 3], max: [5, 5] },
-  { min: [6, 3], max: [8, 5] },
-
-  { min: [0, 6], max: [2, 8] },
-  { min: [3, 6], max: [5, 8] },
-  { min: [6, 6], max: [8, 8] },
 ];
+*/
+
+const range = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function getSquare(puzzle, rowIndex, columnIndex) {
+  const res = [];
+
+  // получение начальных значений квадрата
+  const startRowIndex = rowIndex - (rowIndex % 3);
+  const startColumnIndex = columnIndex - (columnIndex % 3);
+
+  for (let i = 0; i < 3; i += 1) {
+    for (let j = 0; j < 3; j += 1) {
+      res.push(puzzle[startRowIndex + i][startColumnIndex + j]);
+    }
+  }
+
+  return res;
+}
+
+function getRow(puzzle, rowIndex) {
+  return puzzle[rowIndex];
+}
+
+function getColumn(puzzle, columnIndex) {
+  return puzzle.map(row => row[columnIndex]);
+}
+
+function uniq(...args) {
+  const res = [];
+
+  for (let values of args) {
+    for (let value of values) {
+      if (!res.includes(value) && value !== 0) {
+        res.push(value);
+      }
+    }
+  }
+
+  return res;
+}
+
+function getPossible(impossible) {
+  return range.filter(v => !impossible.includes(v));
+}
+
+function isValid(puzzle) {
+  for (let row of puzzle) {
+    if (row.includes(0)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function getNext(rowNum = 0, colNum = 0) {
+  if (colNum !== 8) {
+    return [rowNum, colNum + 1];
+  }
+
+  if (rowNum !== 8) {
+    return [rowNum + 1, 0];
+  }
+
+  return [0, 0];
+}
 
 function toString(puzzle) {
   let str = '-'.repeat(puzzle.length * 3) + '\n';
@@ -85,99 +128,38 @@ function toString(puzzle) {
   }
 
   return str;
-};
-
-function getPossibleColumn(puzzle, y) {
-  const columnValues = map(puzzle, y);
-
-  return range
-    .filter(n => !columnValues.includes(n));
 }
 
-function getPossibleRow(puzzle, x) {
-  const rowValues = puzzle[x];
+function main(puzzle, rowNum = 0, colNum = 0) {
+  if (puzzle[rowNum][colNum] !== 0) {
+    const [nextRowNum, nextColumnNum] = getNext(rowNum, colNum);
+    return main(puzzle, nextRowNum, nextColumnNum);
+  }
 
-  return range
-    .filter(n => !rowValues.includes(n));
-}
+  const square = getSquare(puzzle, rowNum, colNum);
+  const row = getRow(puzzle, rowNum);
+  const column = getColumn(puzzle, colNum);
+  const pos = getPossible(uniq(square, row, column));
 
-function getSquare(x, y) {
-  for (let section of SECTION_LIST) {
-    if (y >= section.min[0] && y <= section.max[0] && x >= section.min[1] && x <= section.max[1]) {
-      return section;
-    }
+  if (pos.length === 1) {
+    puzzle[rowNum][colNum] = +pos;
+  }
+
+  let nextRowNum = rowNum, nextColNum = colNum;
+
+  while (!isValid(puzzle)) {
+    [nextRowNum, nextColNum] = getNext(nextRowNum, nextColNum);
+    main(puzzle, nextRowNum, nextColNum);
   }
 }
 
-function getPossibleSection(puzzle, section) {
-  const numbers = [];
+main(puzzle);
 
-  for (let x = section.min[0]; x <= section.max[0]; x++) {
-    for (let y = section.min[1]; y <= section.max[1]; y++) {
-      numbers.push(puzzle[x][y]);
-    }
-  }
-
-  return range.filter(n => !numbers.includes(n));
-}
-
-function isValid(puzzle) {
-  return flatten(puzzle).filter(n => n === 0).length === 0;
-}
-
-function main(puzzle) {
-  const length = puzzle.length;
-  const result = [...puzzle];
-
-  for (let i = 1; i < 10; i += 1) {
-
-    for (let x = 0; x < length; x += 1) {
-      dance:
-      for (let y = 0; y < length; y += 1) {
-        if (result[y][x] !== 0) {
-          continue dance;
-        }
-
-        const section = getSquare(x, y);
-
-        const rowValues = getPossibleRow(result, y);
-        const colValues = getPossibleColumn(result, x);
-        const secValues = getPossibleSection(result, section);
-
-        const possibleValues = intersection(rowValues, colValues, secValues);
-
-        if (possibleValues.length === 1 && i === possibleValues[0]) {
-          result[y][x] = i;
-        }
-
-        counter += 1;
-      }
-    }
-  }
-
-  return puzzle;
-}
-
-let result = [...puzzle];
-let counter = 0;
-let iter = 0;
-
-while (!isValid(result)) {
-  result = main(result);
-  iter += 1;
-
-  if (iter >= 10) {
-    console.log('limit exceeded');
-    break;
-  }
-}
-
-console.log(toString(result));
-console.log('counter = %d, iter = %d', counter, iter);
+console.log(toString(puzzle));
 
 /*
 >>
-
+---------------------------
 5 3 4  |  6 7 8  |  9 1 2  |
 6 7 2  |  1 9 5  |  3 4 8  |
 1 9 8  |  3 4 2  |  5 6 7  |
@@ -190,6 +172,4 @@ console.log('counter = %d, iter = %d', counter, iter);
 2 8 7  |  4 1 9  |  6 3 5  |
 3 4 5  |  2 8 6  |  1 7 9  |
 ---------------------------
-
-counter = 1283, iter = 6
 */
